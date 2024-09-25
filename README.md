6e97db6fc45b8ed77989361a7eb1ff3dc4607967
Vytautas Šaltenis <vytas@rtfb.lt>
2024-09-25 11:29:59 +0300

    Verbatim copy of my previous TT05 design
    This is a copy of [1], except for the changes in the layout of files and
    directories.
    
    Another difference is that it's missing this change that used to go to
    `src/config.tcl`, and now is supposed to go to `src/config.json`. I'm
    not sure at this point I will need it, so I'm excluding it.
    
        -set ::env(PL_TARGET_DENSITY) 0.6
        +set ::env(PL_TARGET_DENSITY) 0.65
    
    [1]: https://github.com/rtfb/tt05-collatz

--- README.md
+++ README.md
@@ -1,29 +1,61 @@
 ![](../../workflows/gds/badge.svg) ![](../../workflows/docs/badge.svg) ![](../../workflows/test/badge.svg) ![](../../workflows/fpga/badge.svg)
 
-# Tiny Tapeout Verilog Project Template
+# Collatz conjecture brute-forcer
 
-- [Read the documentation for project](docs/info.md)
+A hardware implementation of a brute-force checker for [Collatz conjecture][1].
+It takes an integer number as an input and computes the Collatz sequence until
+it reaches 1. When it does, it allows reading back two numbers:
+1) The orbit length (i.e. the number of steps it took to reach 1)
+2) The highest recorded value of the upper 16 bits of the 144-bit internal iterator
+
+The latter number is an indicator for good candidates for computing [path
+records][2]. The non-zero upper bits indicate that the highest iterator value
+`Mx(N)` is in the range of the previous path records and should be recomputed in
+the full offline. (Holding on to the entire 144 bits of `Mx(N)` number would be
+more obvious, but this almost doubles the footprint of the design, hence, this
+optimisation).
+
+## Using the chip
+
+The module can be in 2 states: IO and COMPUTE. After reset, the chip will be in
+IO mode. Since the input is intended to be much larger than the available pins,
+the input number is uploaded one byte at a time, increasing the address of where
+in the internal 144-bit-wide register that byte should be stored.
+
+Same for reading the output, except that the output numbers are limited to
+16-bits each, so it takes much fewer operations to read them.
+
+### I/O pins should be used as follows:
+
+`ui_in`: an input byte when uploading an input number.
+
+`uo_out`: an output byte when downloading one of the results.
+
+Bidirectional pins are mode-dependant.
+
+When in COMPUTE mode, `uio_out[7]` indicates whether the compute module is busy.
+When it becomes 0, the module will switch to the I/O mode, allowing to read
+output and set a new input. All other bits are meaningless in this mode.
+
+When in IO mode, the pins act as follows:
+| PIN           | Description                                                |
+| ------------- | ---------------------------------------------------------- |
+| `uio_in[7]`   | pulse a 1 to write `ui_in` to the address in `uio_in[4:0]` |
+| `uio_in[6]`   | pulse a 1 to switch to COMPUTE mode                        |
+| `uio_in[5]`   | set to 0 to read orbit length, set to 1 to read the `Mx(N)` upper bits |
+| `uio_in[4:0]` | set to the address to write to or read from                |
+
+See [`set_input()`][3], [`done_computing()`][4] and [`read_n_byte_num()`][5]
+test functions for an example of using this I/O protocol.
 
 ## What is Tiny Tapeout?
 
-Tiny Tapeout is an educational project that aims to make it easier and cheaper than ever to get your digital and analog designs manufactured on a real chip.
+TinyTapeout is an educational project that aims to make it easier and cheaper
+than ever to get your digital designs manufactured on a real chip.
 
 To learn more and get started, visit https://tinytapeout.com.
 
-## Set up your Verilog project
-
-1. Add your Verilog files to the `src` folder.
-2. Edit the [info.yaml](info.yaml) and update information about your project, paying special attention to the `source_files` and `top_module` properties. If you are upgrading an existing Tiny Tapeout project, check out our [online info.yaml migration tool](https://tinytapeout.github.io/tt-yaml-upgrade-tool/).
-3. Edit [docs/info.md](docs/info.md) and add a description of your project.
-4. Adapt the testbench to your design. See [test/README.md](test/README.md) for more information.
-
-The GitHub action will automatically build the ASIC files using [OpenLane](https://www.zerotoasiccourse.com/terminology/openlane/).
-
-## Enable GitHub actions to build the results page
-
-- [Enabling GitHub Pages](https://tinytapeout.com/faq/#my-github-action-is-failing-on-the-pages-part)
-
-## Resources
+### Resources
 
 - [FAQ](https://tinytapeout.com/faq/)
 - [Digital design lessons](https://tinytapeout.com/digital_design/)
@@ -31,11 +63,17 @@ The GitHub action will automatically build the ASIC files using [OpenLane](https
 - [Join the community](https://tinytapeout.com/discord)
 - [Build your design locally](https://www.tinytapeout.com/guides/local-hardening/)
 
-## What next?
+### What next?
 
 - [Submit your design to the next shuttle](https://app.tinytapeout.com/).
 - Edit [this README](README.md) and explain your design, how it works, and how to test it.
 - Share your project on your social network of choice:
   - LinkedIn [#tinytapeout](https://www.linkedin.com/search/results/content/?keywords=%23tinytapeout) [@TinyTapeout](https://www.linkedin.com/company/100708654/)
   - Mastodon [#tinytapeout](https://chaos.social/tags/tinytapeout) [@matthewvenn](https://chaos.social/@matthewvenn)
-  - X (formerly Twitter) [#tinytapeout](https://twitter.com/hashtag/tinytapeout) [@tinytapeout](https://twitter.com/tinytapeout)
+  - Twitter [#tinytapeout](https://twitter.com/hashtag/tinytapeout) [@tinytapeout](https://twitter.com/tinytapeout)
+
+[1]: https://en.wikipedia.org/wiki/Collatz_conjecture
+[2]: http://www.ericr.nl/wondrous/pathrecs.html
+[3]: https://github.com/rtfb/tt05-collatz/blob/main/src/test.py#L191
+[4]: https://github.com/rtfb/tt05-collatz/blob/main/src/test.py#L206
+[5]: https://github.com/rtfb/tt05-collatz/blob/main/src/test.py#L212
--- src/project.v
+++ src/project.v
@@ -2,26 +2,174 @@
  * Copyright (c) 2024 Your Name
  * SPDX-License-Identifier: Apache-2.0
  */
-
 `default_nettype none
 
-module tt_um_example (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+/*
+The module can be in 2 states: IO, COMPUTE.
+IO state has further 2 modes: INPUT and OUTPUT.
+Bidirectional pins have different roles depending on the state:
+
+COMPUTE
+    uio_oe: 1xxx xxxx
+    uio_out[7] - indicates whether the compute module is busy. When it becomes
+                 0, the module will switch to the I/O mode, allowing to read
+                 output and set a new input. All other bits are meaningless in
+                 this mode.
+IO
+    uio_oe: 00AA AAAA
+    uio_in[7]  - pulse a 1 to write ui_in to AAAAAA addr, set to 0 to indicate reading
+    uio_in[6]  - pulse a 1 to switch to COMPUTE mode
+    AAAAAA     - set to the address to write to or read from. When reading,
+                 the highest bit indicates whether that's an orbit length (0)
+                 or a path record (1), when writing, the highest bit is
+                 ignored.
+*/
+
+parameter STATE_IO = 0;
+parameter STATE_COMPUTE = 1;
+
+// size of the Collatz orbit iterator
+parameter BITS = 144;
+parameter BITS_IDX = BITS - 1;
+
+// size of a register for holding orbit length
+parameter OLEN_BITS = 16;
+parameter OLEN_BITS_IDX = OLEN_BITS - 1;
+
+// size of a register for holding the upper bits of path record
+parameter PLEN_BITS = 16;
+parameter PLEN_BITS_IDX = OLEN_BITS - 1;
+
+// width of the address bits
+parameter ADDR_BITS = 5;
+parameter ADDR_IDX = ADDR_BITS - 1;
+
+module collatz (
+    input  state,
+    input  [BITS_IDX:0] iter,
+    input  [OLEN_BITS_IDX:0] orbit_len,
+    input  [PLEN_BITS_IDX:0] path_record_h16,
+    output busy,
+    output [BITS_IDX:0] next_iter,
+    output [OLEN_BITS_IDX:0] next_orbit_len,
+    output [PLEN_BITS_IDX:0] next_path_record
+);
+    wire is_even = !iter[0];
+    wire comp = state == STATE_COMPUTE;
+    assign next_iter = is_even ?
+                       iter >> 1 :
+                       (iter << 1) + iter + 1;
+
+    // XXX: this is a hack: I'm comparing to 2 here to compensate for an
+    // off-by-one bug that I don't understand yet.
+    //
+    // The check for orbit_len maxing out is a safeguard against a dead loop
+    // due to potential unknown bugs.
+    assign busy = iter != 2 && orbit_len != 16'hffff;
+
+    wire [PLEN_BITS_IDX:0] next_iter_h16;
+    assign next_iter_h16 = next_iter[BITS_IDX -: OLEN_BITS];
+    assign next_orbit_len = comp ? orbit_len + 1 : orbit_len;
+    assign next_path_record = comp && (next_iter_h16 > path_record_h16) ?
+                              next_iter_h16 :
+                              path_record_h16;
+endmodule
+
+module tt_um_rtfb_collatz (
+    input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
+    output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
+    input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
+    output wire [7:0] uio_out,  // IOs: Bidirectional Output path
+    output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // will go high when the design is enabled
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
+    wire reset = !rst_n;
+    reg [BITS_IDX:0] iter;
+    reg [OLEN_BITS_IDX:0] orbit_len;
+    reg [PLEN_BITS_IDX:0] path_record_h16;
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+    wire [BITS_IDX:0] next_iter;
+    wire [OLEN_BITS_IDX:0] next_orbit_len;
+    wire [PLEN_BITS_IDX:0] next_path_record;
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+    localparam IOCTL_COMPUTE = 8'h80;
+    localparam IOCTL_IO = 8'h00;
 
+    reg state;          // 0 - IO, 1 - COMPUTE
+    wire compute_busy;
+    reg [7:0] ioctl;
+
+    assign uio_oe = ioctl;
+    assign uio_out = {compute_busy, 7'b0};
+
+    wire [7:0] data_in;
+    reg [7:0] data_out;
+    wire state_bit;
+    wire write_enable;
+    wire [ADDR_IDX:0] addr;
+    wire read_path_record;
+    wire switch_to_compute;
+    wire switch_to_io;
+
+    always @(posedge clk)
+    begin
+        if (reset) begin
+            state <= STATE_IO;
+            ioctl <= IOCTL_IO;
+            data_out <= 0;
+            orbit_len <= 0;
+            path_record_h16 <= 0;
+        end else begin
+            if (switch_to_compute) begin
+                ioctl <= IOCTL_COMPUTE;
+                state <= STATE_COMPUTE;
+                path_record_h16 <= iter[BITS_IDX -: OLEN_BITS];
+            end
+            if (switch_to_io) begin
+                ioctl <= IOCTL_IO;
+                state <= STATE_IO;
+            end
+            case (state)
+                STATE_IO: begin
+                    if (write_enable) begin
+                        iter[addr*8 +: 8] <= data_in;
+                    end else begin
+                        if (read_path_record) begin
+                            data_out <= path_record_h16[addr*8 +: 8];
+                        end else begin
+                            data_out <= orbit_len[addr*8 +: 8];
+                        end
+                    end
+                end
+                STATE_COMPUTE: begin
+                    iter <= next_iter;
+                    orbit_len <= next_orbit_len;
+                    path_record_h16 <= next_path_record;
+                end
+            endcase
+        end
+    end
+
+    assign switch_to_compute = !reset && state_bit && state == STATE_IO;
+    assign switch_to_io = !reset && !compute_busy && state == STATE_COMPUTE;
+
+    collatz collatz(
+        .state(state),
+        .iter(iter),
+        .orbit_len(orbit_len),
+        .path_record_h16(path_record_h16),
+        .busy(compute_busy),
+        .next_iter(next_iter),
+        .next_orbit_len(next_orbit_len),
+        .next_path_record(next_path_record)
+    );
+
+    assign data_in = ui_in;
+    assign uo_out = data_out;
+    assign state_bit = uio_in[6];
+    assign write_enable = uio_in[7];
+    assign addr = uio_in[ADDR_IDX:0];
+    assign read_path_record = uio_in[ADDR_IDX+1];
 endmodule
